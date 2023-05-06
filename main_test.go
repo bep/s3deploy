ad5e10ae06617fcd343b8fb74bd4cba007b39ad2
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2023-05-06 11:29:52 +0200

    Rework test scripts

--- main_test.go
+++ main_test.go
@@ -6,15 +6,13 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 	"sort"
-	"strconv"
 	"strings"
 	"testing"
-	"time"
 
 	"github.com/oklog/ulid/v2"
 
@@ -28,13 +26,9 @@ func TestIntegration(t *testing.T) {
 		t.Skip("S3DEPLOY_TEST_KEY not set")
 
 	}
-	testscript.Run(t, testscript.Params{
-		Dir: "testscripts",
-		//UpdateScripts: true,
-		Setup: func(env *testscript.Env) error {
-			return setup(env)
-		},
-	})
+	p := commonTestScriptsParam
+	p.Dir = "testscripts"
+	testscript.Run(t, p)
 }
 
 // Tests in development can be put in "testscripts/unfinished".
@@ -42,15 +36,10 @@ func TestUnfinished(t *testing.T) {
 	if os.Getenv("CI") != "" {
 		t.Skip("skip unfinished tests on CI")
 	}
+	p := commonTestScriptsParam
+	p.Dir = "testscripts/unfinished"
+	testscript.Run(t, p)
 
-	testscript.Run(t, testscript.Params{
-		Dir: "testscripts/unfinished",
-		//TestWork: true,
-		//UpdateScripts: true,
-		Setup: func(env *testscript.Env) error {
-			return setup(env)
-		},
-	})
 }
 
 func TestMain(m *testing.M) {
@@ -62,88 +51,6 @@ func TestMain(m *testing.M) {
 					fmt.Fprintln(os.Stderr, err)
 					return 1
 				}
-				return 0
-			},
-
-			// head executes HTTP HEAD on the given URL and prints the response status code and
-			// headers to stdout.
-			"head": func() int {
-				url := s3IntegrationTestHttpRoot + os.Args[1]
-				fmt.Println("head", url)
-				resp, err := http.DefaultClient.Head(url)
-				if err != nil {
-					log.Fatal(err)
-				}
-				path := strings.ReplaceAll(os.Args[1], os.Getenv("S3DEPLOY_TEST_ID"), "S3DEPLOY_TEST_ID")
-				fmt.Printf("Head: %s;Status: %d;", path, resp.StatusCode)
-				// Print headers
-				var headers []string
-				for k, v := range resp.Header {
-					headers = append(headers, fmt.Sprintf("%s: %s", k, v[0]))
-				}
-				sort.Strings(headers)
-				fmt.Printf("Headers: %s", strings.Join(headers, ";"))
-
-				return 0
-			},
-
-			// append appends to a file with a leaading newline.
-			"append": func() int {
-				if len(os.Args) < 3 {
-
-					fmt.Fprintln(os.Stderr, "usage: append FILE TEXT")
-					return 1
-				}
-
-				filename := os.Args[1]
-				words := os.Args[2:]
-				for i, word := range words {
-					words[i] = strings.Trim(word, "\"")
-				}
-				text := strings.Join(words, " ")
-
-				_, err := os.Stat(filename)
-				if err != nil {
-					if os.IsNotExist(err) {
-						fmt.Fprintln(os.Stderr, "file does not exist:", filename)
-						return 1
-					}
-					fmt.Fprintln(os.Stderr, err)
-					return 1
-				}
-
-				f, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY, 0o644)
-				if err != nil {
-					fmt.Fprintln(os.Stderr, "failed to open file:", filename)
-					return 1
-				}
-				defer f.Close()
-
-				_, err = f.WriteString("\n" + text)
-				if err != nil {
-					fmt.Fprintln(os.Stderr, "failed to write to file:", filename)
-					return 1
-				}
-
-				return 0
-			},
-
-			// log prints to stderr.
-			"log": func() int {
-				log.Println(os.Args[1])
-				return 0
-			},
-
-			"sleep": func() int {
-				i, err := strconv.Atoi(os.Args[1])
-				if err != nil {
-					i = 1
-				}
-				time.Sleep(time.Duration(i) * time.Second)
-				return 0
-			},
-			"unset": func() int {
-				os.Unsetenv(os.Args[1])
 				return 0
 			},
 		}),
@@ -158,4 +65,82 @@ func setup(env *testscript.Env) error {
 	env.Setenv("S3DEPLOY_TEST_URL", s3IntegrationTestHttpRoot)
 	env.Setenv("S3DEPLOY_TEST_ID", strings.ToLower(ulid.Make().String()))
 	return nil
+}
+
+var commonTestScriptsParam = testscript.Params{
+	Setup: func(env *testscript.Env) error {
+		return setup(env)
+	},
+	Cmds: map[string]func(ts *testscript.TestScript, neg bool, args []string){
+
+		// head executes HTTP HEAD on the given URL and prints the response status code and
+		// headers to stdout.
+		"head": func(ts *testscript.TestScript, neg bool, args []string) {
+			url := s3IntegrationTestHttpRoot + args[0]
+			fmt.Fprintln(ts.Stdout(), "head", url)
+			resp, err := http.DefaultClient.Head(url)
+			if err != nil {
+				ts.Fatalf("failed to HEAD %s: %v", url, err)
+			}
+			path := strings.ReplaceAll(args[0], ts.Getenv("S3DEPLOY_TEST_ID"), "S3DEPLOY_TEST_ID")
+			fmt.Fprintf(ts.Stdout(), "Head: %s;Status: %d;", path, resp.StatusCode)
+			// Print headers
+			var headers []string
+			for k, v := range resp.Header {
+				headers = append(headers, fmt.Sprintf("%s: %s", k, v[0]))
+			}
+			sort.Strings(headers)
+			fmt.Fprintf(ts.Stdout(), "Headers: %s", strings.Join(headers, ";"))
+
+		},
+
+		// append appends to a file with a leaading newline.
+		"append": func(ts *testscript.TestScript, neg bool, args []string) {
+			if len(args) < 2 {
+				ts.Fatalf("usage: append FILE TEXT")
+			}
+
+			filename := ts.MkAbs(args[0])
+			words := args[1:]
+			for i, word := range words {
+				words[i] = strings.Trim(word, "\"")
+			}
+			text := strings.Join(words, " ")
+
+			_, err := os.Stat(filename)
+			if err != nil {
+				if os.IsNotExist(err) {
+					ts.Fatalf("file does not exist: %s", filename)
+				}
+				ts.Fatalf("failed to stat file: %v", err)
+			}
+
+			f, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY, 0o644)
+			if err != nil {
+				ts.Fatalf("failed to open file: %v", err)
+			}
+			defer f.Close()
+
+			_, err = f.WriteString("\n" + text)
+			if err != nil {
+				ts.Fatalf("failed to write to file: %v", err)
+			}
+		},
+		// replace replaces a string in a file.
+		"replace": func(ts *testscript.TestScript, neg bool, args []string) {
+			if len(args) < 3 {
+				ts.Fatalf("usage: replace FILE OLD NEW")
+			}
+			filename := ts.MkAbs(args[0])
+			oldContent, err := os.ReadFile(filename)
+			if err != nil {
+				ts.Fatalf("failed to read file %v", err)
+			}
+			newContent := bytes.Replace(oldContent, []byte(args[1]), []byte(args[2]), -1)
+			err = os.WriteFile(filename, newContent, 0o644)
+			if err != nil {
+				ts.Fatalf("failed to write file: %v", err)
+			}
+		},
+	},
 }
